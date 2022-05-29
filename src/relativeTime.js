94991542962f75e8d07d77943e1f76c8da70ec76
Derek Mason <3derekmason@gmail.com>
2022-05-29 15:09:20 -0600

    test passing returns string

--- src/relativeTime.js
+++ src/relativeTime.js
@@ -1,24 +1,25 @@
 const { NO_DATE } = require("./Errors");
 
-// Input
-// Some date string or number(timestamp),
-// (optional) years, months, days, hours, minutes, seconds
-// (optional) locale region key
-
-// Output
-// A string of the desired interval of time since given date.
-
-// Edge
-// new Date() ex: Sun May 29 2022 14:23:54 GMT-0600 (Mountain Daylight Time)
-// Date.now() ex: 1653855887230
-
-// Constraints
+//  -- Input --
+//  Some date string or number(timestamp),
+//  (optional) years, months, days, hours, minutes, seconds
+//  (optional) locale region key
+//  -- Output --
+//  A string of the desired interval of time since given date.
+//  -- Edge --
+//  new Date() ex: Sun May 29 2022 14:23:54 GMT-0600 (Mountain Daylight Time)
+//  Date.now() ex: 1653855887230
+//  -- Constraints --
 
 const relativeTime = (date, string) => {
+  if (!date) {
+    return NO_DATE;
+  }
   if (typeof date === "number") {
     date = new Date(date);
   }
-  return date ? date : NO_DATE;
+  const timeString = date.toLocaleString();
+  return timeString;
 };
 
 module.exports = relativeTime;
--- test/relativeTime.test.js
+++ test/relativeTime.test.js
@@ -1,13 +1,17 @@
 const relativeTime = require("../src/relativeTime");
 const { NO_DATE } = require("../src/Errors");
 
+const input = new Date();
+const parsed = Date.parse(input);
+
 test("Returns error if not given a date", () => {
   expect(relativeTime()).toBe(NO_DATE);
 });
 test("Parses date input correctly", () => {
-  let input = new Date();
-  let parsed = Date.parse(input);
-
   expect(typeof relativeTime(parsed)).not.toBe("number");
   expect(typeof relativeTime(input)).not.toBe("number");
+});
+test("Returns a string", () => {
+  expect(typeof relativeTime(input)).toBe("string");
+  expect(typeof relativeTime(parsed)).toBe("string");
 });
