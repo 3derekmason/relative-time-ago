08d7317b549af3db2deb8496038d10b0ea4949a5
Derek Mason <3derekmason@gmail.com>
2022-05-30 08:40:50 -0600

    refactor function for readablity and edge cases

--- relative-time-ago.js
+++ relative-time-ago.js
@@ -2,30 +2,63 @@ const milliYears = (n) => {
   return Math.floor(n * 0.000000000031689);
 };
 const milliMonths = (n) => {
-  returnMath.floor(n * 0.00000000038026);
+  return Math.floor(n * 0.00000000038026);
 };
 const milliDays = (n) => {
-  returnMath.floor(n * 0.000000011574);
+  return Math.floor(n * 0.000000011574);
 };
 const milliHours = (n) => {
-  returnMath.floor(n * 0.00000027778);
+  return Math.floor(n * 0.00000027778);
 };
 const milliMinutes = (n) => {
-  returnMath.floor(n * 0.0000166667);
+  return Math.floor(n * 0.0000166667);
 };
 const milliSecs = (n) => {
   return Math.floor(n * 0.001);
 };
 
+const relativeTimeAgo = (date, key) => {
+  if (!date) {
+    return new Error("Doesnt work without a date to reference!");
+  }
+  const currentTimestamp = Date.parse(new Date());
+  if (typeof date !== "number") {
+    date = Date.parse(date);
+  }
+  const difference = currentTimestamp - date;
+  const lessThanMessage = `Less than one ${key} ago`;
 
-const relativeTimeAgo = (date, str) => {
-    const currentTimestamp = Date.parse(new Date());
-    if (typeof date !== "number") {
-      date = Date.parse(date);
-    }
-    const difference = currentTimestamp - date;
+  if (key === "year") {
+    return milliYears(difference) < 1
+      ? lessThanMessage
+      : `${milliYears(difference)} years ago`;
+  }
+  if (key === "month") {
+    return milliMonths(difference) < 1
+      ? lessThanMessage
+      : `${milliMonths(difference)} months ago`;
+  }
+  if (key === "day") {
+    return milliDays(difference) < 1
+      ? lessThanMessage
+      : `${milliDays(difference)} days ago`;
+  }
+  if (key === "hour") {
+    return milliHours(difference) < 1
+      ? lessThanMessage
+      : `${milliHours(difference)} hours ago`;
+  }
+  if (key === "minute") {
+    return milliMinutes(difference) < 1
+      ? lessThanMessage
+      : `${milliMinutes(difference)} minutes ago`;
+  }
+  if (key === "second") {
+    return milliSecs(difference) < 1
+      ? lessThanMessage
+      : `${milliSecs(difference)} seconds ago`;
+  }
+  return `${difference} milliseconds ago`;
+};
 
-    return `Here you go! ${difference}`;
-  },
-
-  export default relativeTimeAgo;
+export default relativeTimeAgo;
--- test.js
+++
@@ -1,6 +0,0 @@
-import relativeTimeAgo = "./relative-time-ago";
-
-const date1 = new Date();
-const date2 = 7676543212;
-
-console.log(date1);
