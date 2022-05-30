175b3df68658e536106f678ae61c881f16f36408
Derek Mason <3derekmason@gmail.com>
2022-05-30 07:37:35 -0600

    new edition

--- relative-time-ago.js
+++ relative-time-ago.js
@@ -1,41 +1,31 @@
-module.exports = {
-  relativeTimeAgo: (date, str) => {
-    const lessThanMessage = `less than a ${str} ago`;
+const milliYears = (n) => {
+  return Math.floor(n * 0.000000000031689);
+};
+const milliMonths = (n) => {
+  returnMath.floor(n * 0.00000000038026);
+};
+const milliDays = (n) => {
+  returnMath.floor(n * 0.000000011574);
+};
+const milliHours = (n) => {
+  returnMath.floor(n * 0.00000027778);
+};
+const milliMinutes = (n) => {
+  returnMath.floor(n * 0.0000166667);
+};
+const milliSecs = (n) => {
+  return Math.floor(n * 0.001);
+};
+
+
+const relativeTimeAgo = (date, str) => {
     const currentTimestamp = Date.parse(new Date());
     if (typeof date !== "number") {
       date = Date.parse(date);
     }
     const difference = currentTimestamp - date;
-    return str === "year"
-      ? Math.floor(difference * 0.000000000031689) < 1
-        ? lessThanMessage
-        : Math.floor(difference * 0.000000000031689)
-      : str === "month"
-      ? Math.floor(difference * 0.00000000038026) < 1
-        ? lessThanMessage
-        : Math.floor(difference * 0.00000000038026)
-      : str === "day"
-      ? Math.floor(difference * 0.000000011574) < 1
-        ? lessThanMessage
-        : Math.floor(difference * 0.000000011574)
-      : str === "hour"
-      ? Math.floor(difference * 0.00000027778) < 1
-        ? lessThanMessage
-        : Math.floor(difference * 0.00000027778)
-      : str === "minute"
-      ? Math.floor(difference * 0.0000166667) < 1
-        ? lessThanMessage
-        : Math.floor(difference * 0.0000166667)
-      : str === "second"
-      ? Math.floor(difference * 0.001) < 1
-        ? lessThanMessage
-        : Math.floor(difference * 0.001)
-      : `${Math.floor(difference * 0.000000000031689)} years, ${Math.floor(
-          difference * 0.00000000038026
-        )} months, ${Math.floor(
-          difference * 0.000000011574
-        )} days, ${Math.floor(difference * 0.00000027778)} hours, ${Math.floor(
-          difference * 0.0000166667
-        )} minutes, and ${Math.floor(difference * 0.001)} seconds ago...`;
+
+    return `Here you go! ${difference}`;
   },
-};
+
+  export default relativeTimeAgo;
---
+++ test.js
@@ -0,0 +1,6 @@
+import relativeTimeAgo = "./relative-time-ago";
+
+const date1 = new Date();
+const date2 = 7676543212;
+
+console.log(date1);
